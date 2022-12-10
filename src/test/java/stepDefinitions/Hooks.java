48b884c79a51257788de91ff16cc80ae719b3348
nglcode <nglcode@gmail.com>
2022-12-10 12:08:39 +0100

    improvements on report, rerun failed scenarios

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,6 +1,12 @@
 package stepDefinitions;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
+import io.cucumber.java.Scenario;
 import utils.TestContextSetup;
 
 public class Hooks {
@@ -14,6 +20,15 @@ public class Hooks {
 	@After
 	public void AfterScenario() {
 		testContextSetup.testBase.WebDriverManager().quit();
+	}
+	
+	@AfterStep
+	public void AddScreenshot(Scenario scenario) {
+		WebDriver driver = testContextSetup.testBase.WebDriverManager();
+		if (scenario.isFailed()) {
+			byte[] sourcePath = ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(sourcePath, "image/png", "image");
+		}
 	}
 
 }
